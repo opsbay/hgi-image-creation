2c44b90cf238a6a8c493e2a4d29f0d6a0846e4df
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-19 17:02:48 +0000

    fix clone-repos.sh and adds logging of what it is doing:

--- scripts/clone-repos.sh
+++ scripts/clone-repos.sh
@@ -7,13 +7,16 @@ clone_dir=$2
 
 echo "clone-repos.sh will clone all repos specified in ${repos_dir} into subdirectories of ${clone_dir}"
 
-mkdir -p ${clone_dir}
-cd ${clone_dir}
-
-for repo_file in ${repos_dir}/*.giturl; do 
-    repo_name="$(basename ${repo_file} .giturl)"
-    repo_url="$(cat ${repo_file})"
-    git clone "${repo_url}" "${repo_name}"
-    cd "${repo_name}"
-    git describe --tags --dirty --always
+mkdir -p "${clone_dir}"
+for repo_file in $(find "${repos_dir}" -maxdepth 1 -name \*.giturl); do
+    (
+        repo_name="$(basename ${repo_file} .giturl)"
+        repo_url="$(cat ${repo_file})"
+        cd "${clone_dir}"
+        echo "cloning from URL: ${repo_url}"
+        git clone "${repo_url}" "${repo_name}"
+        cd "${repo_name}"
+        echo -n "cloned ${repo_name}: "
+        git describe --tags --dirty --always
+    )
 done
