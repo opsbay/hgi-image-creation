41675bf7e8379aff47ae2d3c364f3cc25ef6a335
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-23 13:42:59 +0000

    move ansible_cleanup.sh to packer/scripts

--- packer/scripts/ansible_cleanup.sh
+++ packer/scripts/ansible_cleanup.sh
@@ -1,1 +1,8 @@
-../../subrepos/image-creation/packer/scripts/ansible_cleanup.sh
+#!/bin/bash
+
+set -euf -o pipefail
+
+echo -n "Cleaning up ansible temporary directory ~/.ansible/tmp... "
+rm -rf ~/.ansible/tmp
+echo "done!"
+
--- scripts/ansible_cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euf -o pipefail
-
-echo -n "Cleaning up ansible temporary directory ~/.ansible/tmp... "
-rm -rf ~/.ansible/tmp
-echo "done!"
-
