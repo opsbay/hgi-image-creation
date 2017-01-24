1864be49c227ad369dec6a70dcc71b0df35c5439
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-01-24 15:17:10 +0000

    add additional 2s wait after cloud-init is finished

--- subrepos/image-creation/packer/scripts/cloud-init-wait.sh
+++ subrepos/image-creation/packer/scripts/cloud-init-wait.sh
@@ -5,8 +5,11 @@ set -euf -o pipefail
 timeout_s=$1
 
 echo "waiting ${timeout_s} seconds for cloud-init to finish..."
-timeout ${timeout_s} /bin/bash -c 'until stat /var/lib/cloud/instance/boot-finished 2>/dev/null; do echo -n "."; sleep 1; done' || echo "failed!"
+timeout ${timeout_s} /bin/bash -c 'until stat /var/lib/cloud/instance/boot-finished &>/dev/null; do echo -n "."; sleep 1; done' || echo "failed!"
 echo "done!"
 echo -n "cloud-init finished: "
 cat /var/lib/cloud/instance/boot-finished
 
+echo -n "waiting 2s more for good measure... "
+sleep 2
+echo "done!"
