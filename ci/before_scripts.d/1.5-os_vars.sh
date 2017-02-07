c85064ab9925e7f81ebb9fc4c3542d8247a251bd
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-07 13:28:46 +0000

    change 1.5-os_vars.sh to log $BASH_SOURCE instead of $0

--- ci/before_scripts.d/1.5-os_vars.sh
+++ ci/before_scripts.d/1.5-os_vars.sh
@@ -1,17 +1,17 @@
 
 if [ "${CI_BUILD_STAGE}" == "deploy" ]; then
-    echo "$0 setting OS deploy variables"
+    echo "$BASH_SOURCE setting OS deploy variables"
     export OS_AUTH_URL="$(echo ${DEPLOY_OS_AUTH_URL} | sed 's/[\r\n]//g')"
     export OS_USERNAME="$(echo ${DEPLOY_OS_USERNAME} | sed 's/[\r\n]//g')"
     export OS_PASSWORD="$(echo ${DEPLOY_OS_PASSWORD} | sed 's/[\r\n]//g')"
-    echo "Using OS_AUTH_URL=[${OS_AUTH_URL}] OS_USERNAME=[${OS_USERNAME}]"
+    echo "$BASH_SOURCE using OS_AUTH_URL=[${OS_AUTH_URL}] OS_USERNAME=[${OS_USERNAME}]"
 else
-    echo "$0 setting OS packer variables"
+    echo "$BASH_SOURCE setting OS packer variables"
     export OS_AUTH_URL="$(echo ${PACKER_OS_AUTH_URL} | sed 's/[\r\n]//g')"
     export OS_TENANT_NAME="$(echo ${PACKER_OS_TENANT_NAME} | sed 's/[\r\n]/)/g')"
     export OS_USERNAME="$(echo ${PACKER_OS_USERNAME} | sed 's/[\r\n]//g')"
     export OS_PASSWORD="$(echo ${PACKER_OS_PASSWORD} | sed 's/[\r\n]//g')"
     export OS_NETWORKS="$(echo ${PACKER_OS_NETWORKS} | sed 's/[\r\n]//g')"
-    echo "Using OS_AUTH_URL=[${OS_AUTH_URL}] OS_USERNAME=[${OS_USERNAME}] OS_TENANT_NAME=[${OS_TENANT_NAME}] OS_NETWORKS=[${OS_NETWORKS}]"
+    echo "$BASH_SOURCE using OS_AUTH_URL=[${OS_AUTH_URL}] OS_USERNAME=[${OS_USERNAME}] OS_TENANT_NAME=[${OS_TENANT_NAME}] OS_NETWORKS=[${OS_NETWORKS}]"
 fi
 
