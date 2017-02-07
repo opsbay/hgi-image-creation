4e939a3b2c24772df109d4df0730d5696299d1aa
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-02-07 12:59:33 +0000

    add missing export to 1.5-os_vars.sh and move to hgi-image-creation subrepo

---
+++ ci/before_scripts.d/1.5-os_vars.sh
@@ -0,0 +1,10 @@
+if [ "${CI_BUILD_STAGE}" == "deploy" ]; then
+    export OS_AUTH_URL="${DEPLOY_OS_AUTH_URL}"
+    export OS_USERNAME="${DEPLOY_OS_USERNAME}"
+    export OS_PASSWORD="${DEPLOY_OS_PASSWORD}"
+else
+    export OS_AUTH_URL="${PACKER_OS_AUTH_URL}"
+    export OS_TENANT_NAME="${PACKER_OS_TENANT_NAME}"
+    export OS_USERNAME="${PACKER_OS_USERNAME}"
+    export OS_PASSWORD="${PACKER_OS_PASSWORD}"
+fi
