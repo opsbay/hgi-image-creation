7640fcac9e84b924d615c585fc2064c811386015
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-02-08 12:27:57 +0000

    Sorts out Test Kitchen variables.

--- ci/before_scripts.d/1-default.sh
+++ ci/before_scripts.d/1-default.sh
@@ -31,3 +31,11 @@ export PACKER_ANSIBLE_DIR="${HGI_SYSTEMS_DIR}/ansible"
 export ARTIFACTS_DIR="${GITLAB_ARTIFACTS_DIR}"
 export OS_SOURCE_IMAGE_ARTIFACT="${GITLAB_OS_SOURCE_IMAGE_ARTIFACT}"
 export OS_NETWORKS_ARTIFACT="${GITLAB_OS_NETWORKS_ARTIFACT}"
+
+export KITCHEN_WRAPPER="${IMAGE_CREATION_DIR}/kitchen_wrapper.sh"
+export KITCHEN_YAML="${IMAGE_CREATION_DIR}/.kitchen.yml"
+export KITCHEN_PROVISIONER_SCRIPT="${IMAGE_CREATION_DIR}/bootstrap.sh"
+export KITCHEN_DEBUG="0"    # "0" for normal logging or "1" for extra logging and allowing ssh to instance for 20 mins if tests fail
+export KITCHEN_MODE=default
+export KITCHEN_OS_NETWORKS="${OS_NETWORKS}"
+export KITCHEN_OS_SECURITY_GROUPS="${OS_SECURITY_GROUPS}"
