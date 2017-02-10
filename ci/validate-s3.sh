6c4f4cf84f5f4632c71602e567f18c84963f22db
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-02-10 15:46:18 +0000

    Requires S3 image bucket to be defined.

--- ci/validate-s3.sh
+++ ci/validate-s3.sh
@@ -18,6 +18,10 @@ if [ -z "${S3_HOST_BUCKET+x}" ]; then
     >&2 echo "S3_HOST_BUCKET must be set!"
     exit 1
 fi
+if [ -z "${S3_IMAGE_BUCKET+x}" ]; then
+    >&2 echo "S3_IMAGE_BUCKET must be set!"
+    exit 1
+fi
 
 s3cmd info \
         --access_key="${S3_ACCESS_KEY}" \
@@ -25,4 +29,4 @@ s3cmd info \
         --ssl \
         --host="${S3_HOST}" \
         --host-bucket="${S3_HOST_BUCKET}" \
-    "s3://${S3_HOST_BUCKET}"
+    "s3://${S3_IMAGE_BUCKET}"
