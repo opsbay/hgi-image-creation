70b7844981aab98273bf4cc9857bec792b846b1d
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-02-13 11:08:15 +0000

    Defines source image.

--- test/before_scripts.d/10-overrides.sh
+++ test/before_scripts.d/10-overrides.sh
@@ -4,7 +4,10 @@
 export HGI_IMAGE_CREATION_DIR="./"
 export IMAGE_CREATION_DIR="./subrepos/image-creation"
 
-export PACKER_DIR="${HGI_IMAGE_CREATION_DIR}/packer"
+#export PACKER_DIR="${HGI_IMAGE_CREATION_DIR}/packer"
+
+export OS_SOURCE_IMAGE_xenial="Ubuntu Xenial"
+#export PACKER_OS_SOURCE_IMAGE="	Ubuntu 16.04 Xenial"
 
 export KITCHEN_YAML="${IMAGE_CREATION_DIR}/.kitchen.yml"
 export KITCHEN_PROVISIONER_SCRIPT="${IMAGE_CREATION_DIR}/bootstrap.sh"
