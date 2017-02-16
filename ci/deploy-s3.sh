2853d5ab53832d6c226c32acdbac431b0a21d7dc
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-02-16 14:36:19 +0000

    Corrects name of image that is to be uploaded to S3.

--- ci/deploy-s3.sh
+++ ci/deploy-s3.sh
@@ -5,7 +5,7 @@ set -euf -o pipefail
 IMAGE_FILE_LOCATION="${DEPLOY_IMAGE_NAME}.img"
 
 # Save OpenStack image to disk
-openstack image save --file "${IMAGE_FILE_LOCATION}" "${DEPLOY_IMAGE_NAME}"
+openstack image save --file "${IMAGE_FILE_LOCATION}" "${PACKER_IMAGE_NAME}"
 
 # Upload OpenStack image to S3
 s3cmd put \
