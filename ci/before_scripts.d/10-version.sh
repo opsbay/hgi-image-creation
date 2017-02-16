55d3446c457dded4734f679ded3d7a8066d722a1
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-02-16 11:25:30 +0000

    Improve logging for settings PACKER_IMAGE_NAME.

--- ci/before_scripts.d/10-version.sh
+++ ci/before_scripts.d/10-version.sh
@@ -5,19 +5,32 @@ set +e
 set +u
 eval export OS_SOURCE_IMAGE="\${OS_SOURCE_IMAGE_${DISTRO}}"
 export VERSION=$(${VERSION_COMMAND})
-test -n "${IMAGE_BASENAME}" && test -n "${DISTRO}" && test -n "${VERSION}" && export PACKER_IMAGE_NAME="${IMAGE_BASENAME}-${DISTRO}-${VERSION}"
+if [[ -n "${IMAGE_BASENAME}" ]]; then
+    if [[ -n "${DISTRO}" ]]; then
+        if [[ -n "${VERSION}" ]]; then
+            export PACKER_IMAGE_NAME="${IMAGE_BASENAME}-${DISTRO}-${VERSION}"
+            echo "Set PACKER_IMAGE_NAME to ${PACKER_IMAGE_NAME}"
+        else
+            echo "Not setting PACKER_IMAGE_NAME because VERSION is not set"
+        fi
+    else
+        echo "Not setting PACKER_IMAGE_NAME because DISTRO is not set"
+    fi
+else
+    echo "Not setting PACKER_IMAGE_NAME because IMAGE_BASENAME is not set"
+fi
 export DEPLOY_IMAGE_NAME="${PACKER_IMAGE_NAME}"
 export DEPLOY_IMAGE_NAME_LATEST="${IMAGE_BASENAME}-${DISTRO}-${LATEST_VERSION_PLACEHOLDER}"
 eval export PACKER_SOURCE_IMAGE_USERNAME="\${PACKER_SOURCE_IMAGE_USERNAME_${DISTRO}}"
 
-if [ -r "${OS_SOURCE_IMAGE_ARTIFACT}" ]; then
+if [[ -r "${OS_SOURCE_IMAGE_ARTIFACT}" ]]; then
     export PACKER_OS_SOURCE_IMAGE="$(cat ${OS_SOURCE_IMAGE_ARTIFACT})"
     echo "Set PACKER_OS_SOURCE_IMAGE=${PACKER_OS_SOURCE_IMAGE} from ${OS_SOURCE_IMAGE_ARTIFACT} artifact."
 else
     echo "No readable OS_SOURCE_IMAGE_ARTIFACT (${OS_SOURCE_IMAGE_ARTIFACT})"
 fi
 
-if [ -r "${OS_NETWORKS_ARTIFACT}" ]; then
+if [[ -r "${OS_NETWORKS_ARTIFACT}" ]]; then
     export PACKER_OS_NETWORKS="$(cat ${OS_NETWORKS_ARTIFACT})"
     echo "Set PACKER_OS_NETWORKS=${PACKER_OS_NETWORKS} from ${OS_NETWORKS_ARTIFACT} artifact."
 else
