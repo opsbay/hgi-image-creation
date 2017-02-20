79fcb7d74443c9050a2f48d39d442673ecb20118
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-02-20 11:58:14 +0000

    Adds ability to specify multiple OS source image names (first that exists is used).

--- ci/prepare-os-source-image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu -o pipefail
-
-mkdir -p $(dirname "${GITLAB_OS_SOURCE_IMAGE_ARTIFACT}")
-openstack image show -c id -f value "${OS_SOURCE_IMAGE}" > "${GITLAB_OS_SOURCE_IMAGE_ARTIFACT}"
