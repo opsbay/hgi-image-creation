c83cccb433e999ecd3734be49d58906828d39f56
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-03-05 23:57:18 +0000

    change s3 deployment to pipe through mc so as not to require multiple GB of temporary space on gitlab runners

--- ci/deploy-s3.sh
+++ ci/deploy-s3.sh
@@ -2,16 +2,10 @@
 
 set -euf -o pipefail
 
-# Save OpenStack image to disk
-openstack image save --file "${DEPLOY_IMAGE_NAME}" "${PACKER_IMAGE_NAME}"
+# Stream OpenStack image to stdout and pipe it both into md5sum and minio client for upload to S3
+(openstack image save "${PACKER_IMAGE_NAME}" | tee /dev/fd/5 | mc pipe "deploy/${S3_IMAGE_BUCKET}/${DEPLOY_IMAGE_NAME}") 5>&1 > "${DEPLOY_IMAGE_NAME}.md5"
 
-# Upload OpenStack image to S3
-s3cmd put \
-        --access_key="${S3_ACCESS_KEY}" \
-        --secret_key="${S3_SECRET_KEY}" \
-        --check-md5 \
-        --ssl \
-        --progress \
-        --host="${S3_HOST}" \
-        --host-bucket="${S3_HOST_BUCKET}" \
-    "${DEPLOY_IMAGE_NAME}" "s3://${S3_IMAGE_BUCKET}"
+# fetch data back from s3 and check md5
+mc cat "deploy/${S3_IMAGE_BUCKET}/${DEPLOY_IMAGE_NAME}" | md5sum -c "${DEPLOY_IMAGE_NAME}.md5"
+
+echo "Uploaded ${DEPLOY_IMAGE_NAME} to ${S3_IMAGE_BUCKET} with md5: $(cat ${DEPLOY_IMAGE_NAME}.md5)"
