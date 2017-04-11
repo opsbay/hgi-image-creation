420b4fb639fd8cebbf248f9c1502309ad60a261b
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-04-11 15:00:45 +0100

    Temp workaround for #1.

--- ci/validate-packer.sh
+++ ci/validate-packer.sh
@@ -25,7 +25,7 @@ for secgroup in "${PACKER_OS_SECURITY_GROUPS}"; do openstack security group show
 
 openstack flavor show -c name -c id -f table "${PACKER_OS_FLAVOR}"
 
-openstack ip floating pool list -f value | grep "${PACKER_OS_FLOATING_IP_POOL}"
+neutron net-external-list -f value | grep "${PACKER_OS_FLOATING_IP_POOL}"
 
 test "${PACKER_CLOUD_INIT_WAIT_TIMEOUT_S}" -gt 0
 
