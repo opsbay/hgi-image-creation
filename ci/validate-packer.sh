dd04f6020ffbd3f88aea92d503c53b941bb4fabb
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-05-22 15:27:57 +0100

    git subrepo commit subrepos/hgi-image-creation
    subrepo:
      subdir:   "subrepos/hgi-image-creation"
      merged:   "0871e2e"
    upstream:
      origin:   "https://github.com/wtsi-hgi/hgi-image-creation.git"
      branch:   "master"
      commit:   "449a791"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "55f8cfd"

--- ci/validate-packer.sh
+++ ci/validate-packer.sh
@@ -25,8 +25,9 @@ for secgroup in "${PACKER_OS_SECURITY_GROUPS}"; do openstack security group show
 
 openstack flavor show -c name -c id -f table "${PACKER_OS_FLAVOR}"
 
-echo "Not checking that ${PACKER_OS_FLOATING_IP_POOL} floating ip pool exists because compute v2 is required to query floating ip pools"
-#openstack floating ip pool list -f value | grep "${PACKER_OS_FLOATING_IP_POOL}"
+# FIXME: Disabling check until solution to https://github.com/wtsi-hgi/hgi-image-creation/issues/1 is found
+echo "Not validating exitence of floating ip pool ${PACKER_OS_FLOATING_IP_POOL} (https://github.com/wtsi-hgi/hgi-image-creation/issues/1)"
+# openstack floating ip pool list -f value | grep "${PACKER_OS_FLOATING_IP_POOL}"
 
 test "${PACKER_CLOUD_INIT_WAIT_TIMEOUT_S}" -gt 0
 
