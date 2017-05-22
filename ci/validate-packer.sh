f63a7de66e908354363813ce70930c196804474c
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-05-22 14:22:42 +0100

    change ip floating to floating ip so that it works with openstack 3.9+ clients

--- ci/validate-packer.sh
+++ ci/validate-packer.sh
@@ -25,8 +25,7 @@ for secgroup in "${PACKER_OS_SECURITY_GROUPS}"; do openstack security group show
 
 openstack flavor show -c name -c id -f table "${PACKER_OS_FLAVOR}"
 
-# FIXME: Disabling check until solution to https://github.com/wtsi-hgi/hgi-image-creation/issues/1 is found
-# openstack ip floating pool list -f value | grep "${PACKER_OS_FLOATING_IP_POOL}"
+openstack floating ip pool list -f value | grep "${PACKER_OS_FLOATING_IP_POOL}"
 
 test "${PACKER_CLOUD_INIT_WAIT_TIMEOUT_S}" -gt 0
 
