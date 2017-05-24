dbdad2ab3dcf8cdbb59b90eee031f6688f8aa562
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-05-24 10:53:02 +0100

    Creates artifact directory if it doesn't exist.

--- ci/prepare-os-source-image.sh
+++ ci/prepare-os-source-image.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail
+
 # TODO: The ruby script is going to move to GITLAB_HGI_CI_DIR
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir -p $(dirname "${GITLAB_OS_SOURCE_IMAGE_ARTIFACT}")
 "${SCRIPT_DIRECTORY}/prepare-os-source-image.rb" "${OS_SOURCE_IMAGE}" "${S3_IMAGE_BUCKET}" > "${GITLAB_OS_SOURCE_IMAGE_ARTIFACT}"
