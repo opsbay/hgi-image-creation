587a1d5c4909f419309a4aa816b0f3a03ecfdbc0
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-05-24 11:48:25 +0100

    Switches to using script to prepare OpenStack source image from `hgi-systems`.

--- ci/prepare-os-source-image.sh
+++ ci/prepare-os-source-image.sh
@@ -2,8 +2,7 @@
 
 set -eu -o pipefail
 
-# TODO: The ruby script is going to move to GITLAB_HGI_CI_DIR
-SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 mkdir -p $(dirname "${GITLAB_OS_SOURCE_IMAGE_ARTIFACT}")
-"${SCRIPT_DIRECTORY}/prepare-os-source-image.rb" "${OS_SOURCE_IMAGE}" "${S3_IMAGE_BUCKET}" > "${GITLAB_OS_SOURCE_IMAGE_ARTIFACT}"
+
+"${HGI_IMAGE_CREATION_DIR}/subrepos/hgi-systems/ci/prepare-os-image.rb" "${OS_SOURCE_IMAGE}" "${S3_IMAGE_BUCKET}" \
+    > "${GITLAB_OS_SOURCE_IMAGE_ARTIFACT}"
