b623fc8bd459f15b95d6646eda9e7ec04f8d1887
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-07-11 17:42:54 +0100

    update md5 checks in deploy-s3.sh

--- ci/deploy-s3.sh
+++ ci/deploy-s3.sh
@@ -4,12 +4,20 @@ set -euf -o pipefail
 
 echo ""
 # Stream OpenStack image to stdout and pipe it both into md5sum and minio client for upload to S3
-(openstack image save "${PACKER_IMAGE_NAME}" | tee /dev/fd/4 | mc pipe deploy/${S3_IMAGE_BUCKET}/${DEPLOY_IMAGE_NAME}) 4>&1 | md5sum - > "${DEPLOY_IMAGE_NAME}.md5"
+upload_md5=$((openstack image save "${PACKER_IMAGE_NAME}" | tee /dev/fd/4 | mc pipe deploy/${S3_IMAGE_BUCKET}/${DEPLOY_IMAGE_NAME}) 4>&1 | md5sum - )
 
-echo "Uploaded ${S3_IMAGE_BUCKET}/${DEPLOY_IMAGE_NAME} with md5:"
-cat "${DEPLOY_IMAGE_NAME}.md5"
+echo "Uploaded ${S3_IMAGE_BUCKET}/${DEPLOY_IMAGE_NAME} with md5: ${upload_md5}"
 
 # fetch data back from s3 and check md5
-mc cat deploy/${S3_IMAGE_BUCKET}/${DEPLOY_IMAGE_NAME} | md5sum -c "${DEPLOY_IMAGE_NAME}.md5" -
+check_md5=$(mc cat deploy/${S3_IMAGE_BUCKET}/${DEPLOY_IMAGE_NAME} | md5sum - )
 
-echo "Uploaded ${DEPLOY_IMAGE_NAME} to ${S3_IMAGE_BUCKET} with md5: $(cat ${DEPLOY_IMAGE_NAME}.md5)"
+echo "Fetched ${S3_IMAGE_BUCKET}/${DEPLOY_IMAGE_NAME} with md5: ${check_md5}"
+
+if [[ "${upload_md5}" == "${check_md5}" ]]; then
+    echo "MD5 match"
+else
+    echo "MD5 mismatch"
+    exit 1
+fi
+
+exit 0
