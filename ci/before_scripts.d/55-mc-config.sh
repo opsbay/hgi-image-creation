bc0a53edb91eee7882a81b2192c46404b5151353
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-07-11 18:42:58 +0100

    add mc config ci before script and mc validation to validate-s3.sh

---
+++ ci/before_scripts.d/55-mc-config.sh
@@ -0,0 +1,22 @@
+set -euf -o pipefail
+
+if [[ -z "${S3_ACCESS_KEY+x}" ]]; then
+    >&2 echo "S3_ACCESS_KEY must be set!"
+    exit 1
+fi
+if [[ -z "${S3_SECRET_KEY+x}" ]]; then
+    >&2 echo "S3_SECRET_KEY must be set!"
+    exit 1
+fi
+if [[ -z "${S3_HOST+x}" ]]; then
+    >&2 echo "S3_HOST must be set!"
+    exit 1
+fi
+
+MC_BIN="$(which mc)"
+
+if [[ -z "${MC_BIN}" ]]; then
+    >&2 echo "skipping mc config since mc is not in PATH"
+else
+    mc config host add deploy "${S3_HOST}" "${S3_ACCESS_KEY}" "${S3_SECRET_KEY}"
+fi
--- ci/validate-s3.sh
+++ ci/validate-s3.sh
@@ -2,6 +2,7 @@
 
 set -euf -o pipefail
 
+# validate s3cmd config
 if [ ! -f "${HOME}/.s3cfg" ]; then
     >&2 echo "${HOME}/.s3cfg must be written!"
     exit 1
@@ -20,3 +21,17 @@ if [ -n "${existingImage}" ]; then
     >&2 echo "An image named '${DEPLOY_IMAGE_NAME}' already exists in the object store, refusing to continue!"
     exit 1
 fi
+
+# validate minio client config
+if [ ! -f "${HOME}/.mc/config.json" ]; then
+    >&2 echo "${HOME}/.mc/config.json must exist!"
+    exit 1
+fi
+
+deploy_host_access=$(mc config host list | awk '$1=="deploy:" {print $2, $3}')
+if [ -z "${deploy_host_access}" ]; then
+    >&2 echo "mc config has no entry for deploy"
+    exit 1
+fi
+
+echo "validate-s3.sh: mc config deploy entry using ${deploy_host_access}"
