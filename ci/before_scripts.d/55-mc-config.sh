4e05ae8b6139539b508e9eddd2764947a890f3f9
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-07-11 18:45:26 +0100

    fix before script for s3cmd and mc

--- ci/before_scripts.d/55-mc-config.sh
+++ ci/before_scripts.d/55-mc-config.sh
@@ -1,5 +1,3 @@
-set -euf -o pipefail
-
 if [[ -z "${S3_ACCESS_KEY+x}" ]]; then
     >&2 echo "S3_ACCESS_KEY must be set!"
     exit 1
@@ -13,7 +11,7 @@ if [[ -z "${S3_HOST+x}" ]]; then
     exit 1
 fi
 
-MC_BIN="$(which mc)"
+MC_BIN="$(which mc || true)"
 
 if [[ -z "${MC_BIN}" ]]; then
     >&2 echo "skipping mc config since mc is not in PATH"
