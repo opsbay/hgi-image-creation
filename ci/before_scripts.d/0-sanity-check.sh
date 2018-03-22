c6a9ea8763a16d6ee707414a1304bb46d273587e
Joshua Randall <joshua.randall@sanger.ac.uk>
2018-03-22 17:48:05 +0000

    add which which sanity check

--- ci/before_scripts.d/0-sanity-check.sh
+++ ci/before_scripts.d/0-sanity-check.sh
@@ -1,3 +1,13 @@
+WHICH_BIN=$("which which || true)"
+if [[ -z "${WHICH_BIN}" ]]; then
+    echo "which required but not found in path"
+    exit 1
+fi
+if [[ ! -x "${WHICH_BIN}" ]]; then
+    echo "which is on path (${WHICH_BIN}) but cannot be executed"
+    exit 1
+fi
+
 BASH_BIN=$("which bash || true)"
 if [[ -z "${BASH_BIN}" ]]; then
     echo "bash required but not found in path"
